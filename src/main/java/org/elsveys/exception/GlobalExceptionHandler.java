f7a9ecb47d0ef6c943b9bf6c5abc66cce69483b8
ElsveysPE <ieelsveys@gmail.com>
2025-09-15 08:25:31 +0300

    Bug fixes

--- src/main/java/org/elsveys/exception/GlobalExceptionHandler.java
+++ src/main/java/org/elsveys/exception/GlobalExceptionHandler.java
@@ -14,12 +14,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(PersistenceException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleDatabase(PersistenceException e) {
-        return Map.of("error", e.getCause().getMessage());
+        e.printStackTrace();
+        String message = e.getCause() != null ? e.getCause().getMessage() : e.getMessage();
+        return Map.of("error", message);
     }
-
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Map<String, String> handleGeneral(RuntimeException e) {
+        e.printStackTrace();
         return Map.of("error", e.getMessage());
     }
 }
